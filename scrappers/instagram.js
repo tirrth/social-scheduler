c5084db3f402b0a1c3fabbeeec21a20480461181
Tirth Patel <tirrrthpatel@gmail.com>
2021-05-28 09:55:50 +0530

    Cosmetic changes for testing

--- scrappers/instagram.js
+++ scrappers/instagram.js
@@ -2,11 +2,10 @@ const puppeteer = require("puppeteer");
 const https = require("https");
 const fs = require("fs");
 const os = require("os");
-const path = require("path");
 const ffmpegPath = require("@ffmpeg-installer/ffmpeg").path;
 const ffprobePath = require("@ffprobe-installer/ffprobe").path;
 const ffmpeg = require("fluent-ffmpeg");
-const { generateRandomInteger, minimumIntegerDigits } = require("./util");
+const { generateRandomInteger, minimumIntegerDigits } = require("../util");
 ffmpeg.setFfmpegPath(ffmpegPath);
 ffmpeg.setFfprobePath(ffprobePath);
 
@@ -15,7 +14,7 @@ class InstagramPuppet {
   #EXT_INSSIST_PATH = `${this.#BASE_PATH}/extensions/inssist`;
   #BASE_URL = "https://www.instagram.com";
   #INSTA_MOBILE_URL = "https://insta-mobile.netlify.app";
-  #FALLBACK_IMAGE_URL = null;
+  #FALLBACK_IMAGES_URL = [];
   #BASE_DIR = null;
   #browser = null;
   #page = null;
@@ -35,11 +34,17 @@ class InstagramPuppet {
   };
 
   #downloadStoryToLocal = async ({ url, dest, cb }) => {
+    if (!url) return cb?.("No Story Found!!");
+    const alt_cb = (err) => {
+      if (err) console.log("File Downloading Callback Error: ", err);
+      else console.log("File Downloading Callback Response: Successful....");
+    };
+    cb = cb || alt_cb;
     const file = fs.createWriteStream(dest);
     const request = https.get(url, (response) => {
       // check if response is success
       if (response.statusCode !== 200) {
-        return cb?.("Response status was " + response.statusCode);
+        return cb("Response status was " + response.statusCode);
       }
       response.pipe(file);
     });
@@ -47,13 +52,13 @@ class InstagramPuppet {
     file.on("finish", () => file.close(cb));
     // check for request error too
     request.on("error", (err) => {
-      fs.unlink(dest);
-      return cb?.(err?.message);
+      fs.unlink(dest, () => null);
+      return cb(err?.message);
     });
     file.on("error", (err) => {
       // Handle errors
-      fs.unlink(dest); // Delete the file async. (But we don't check the result)
-      return cb?.(err?.message);
+      fs.unlink(dest, () => null); // Delete the file async. (But we don't check the result)
+      return cb(err?.message);
     });
   };
 
@@ -141,7 +146,7 @@ class InstagramPuppet {
       var page_link = `${this.#BASE_URL}/explore/tags/${page.substring(1)}`;
     } else page_link = `${this.#BASE_URL}/${page}`;
     page_link = `${page_link}?__a=1`;
-    console.log("Page Link =", page_link);
+    console.log("Page Link: " + page_link);
     const response = await this.#page.goto(page_link);
     const body = await response.json();
     const edges = body?.graphql?.user?.edge_owner_to_timeline_media?.edges;
@@ -170,6 +175,7 @@ class InstagramPuppet {
       timeout: 90000, // default: 30000
     });
     this.#removeStoryFromLocal(file_path);
+    console.log("Story uploaded successfully...");
     await cb?.({ success: true, res: "Story uploaded successfully" });
   };
 
@@ -190,7 +196,7 @@ class InstagramPuppet {
           else if (i && i % 3600 === 0) (j = 0), l++;
           else j += 15;
         }
-        console.log("Time intervals =", time_intervals);
+        console.log("Time intervals: ", time_intervals);
         let count = 0;
         time_intervals.map((startTime, idx) => {
           ffmpeg(input)
@@ -200,10 +206,11 @@ class InstagramPuppet {
             .on("end", (err) => {
               if (err) return console.log("Ffmpeg Error: ", err), callback(err);
               count += 1;
-              console.log("Conversion done => " + count);
+              console.log("Conversion count => " + count);
               if (count === time_intervals.length) {
                 this.#removeStoryFromLocal(input);
-                callback(err, "Conversion Done");
+                console.log("--- Conversion Done ---");
+                return callback(err, "Conversion Done");
               }
             })
             .on("error", callback)
@@ -235,8 +242,13 @@ class InstagramPuppet {
   };
 
   uploadStoryFromUrl = (url, options = {}) => {
-    if (!url) options.is_video = false;
-    url = url || this.#FALLBACK_IMAGE_URL;
+    if (!url) {
+      options.is_video = false;
+      const fallback_images = this.#FALLBACK_IMAGES_URL;
+      const random_no = generateRandomInteger(0, fallback_images.length);
+      url = fallback_images?.[random_no] || fallback_images?.[0];
+      console.log("Fallback Story URL: " + url);
+    } else console.log("Story URL: " + url);
     const file_name = `file.${!options.is_video ? "jpeg" : "mp4"}`;
     const file_path = `${this.#BASE_DIR}/${file_name}`;
     this.#downloadStoryToLocal({
@@ -244,6 +256,7 @@ class InstagramPuppet {
       dest: file_path,
       cb: (err) => {
         if (err) return options.callback({ success: false, error: err });
+        console.log("File downloaded successfully...");
         if (options.is_video) {
           this.#uploadVideoFromLocal(file_path, options.callback);
         } else this.#uploadStoryFromLocal(file_path, options.callback);
@@ -251,127 +264,11 @@ class InstagramPuppet {
     });
   };
 
-  setFallbackImage = (img) => (this.#FALLBACK_IMAGE_URL = img);
+  setFallbackImages = (imgs) => this.#FALLBACK_IMAGES_URL.push(...imgs);
 
   setBaseDir = (dir) => (this.#BASE_DIR = dir);
 
   exit = async () => (await this.#browser.close(), this.#resetDirectory());
 }
 
-const memes = [
-  "meme.ig",
-  "fuckjerry",
-  "sarcasm_only",
-  "daquan",
-  "thefatjewish",
-  "lmao",
-  "funnymemes",
-  "nbamemes",
-  "memes",
-  "memezar",
-  "lil_funny_memess",
-];
-
-const motivational = [
-  "6amsuccess",
-  "motivationmafia",
-  "foundr",
-  "addicted2success",
-  "quoteoftheday",
-  "quotes",
-  "positivevibesquotes",
-  "gangsters.quotes",
-  "poetry",
-  "humblethepoet",
-  "visionofsuccess",
-];
-
-const cute_animals = [
-  "cute_animalvideos",
-  "cute.animals",
-  "jiffpom",
-  "pumpkintheraccoon",
-  "this_girl_is_a_squirrel",
-  "hamilton_the_hipster_cat",
-  "reagandoodle",
-  "juniperfoxx",
-  "nala_cat",
-  "lizzie.bear",
-];
-
-const knowledge = [
-  "educating.facts",
-  "amazing_.facts._",
-  "knowledge__iq",
-  "knowledge.hack",
-  "knowledge4genius",
-  "#facts",
-  "psychologyfact_",
-  "facts_and_science",
-  "science_facts_07",
-  "facts.trend",
-];
-
-const nature = [
-  "nature",
-  "#nature",
-  "#naturephotography",
-  "nature.geography",
-  "discovery",
-  "#naturegeography",
-  "nature._.videos",
-];
-
-const _getInstagramPageCategory = () => {
-  const categories = [memes, motivational, cute_animals, knowledge, nature];
-  const random_category_no = generateRandomInteger(0, categories.length - 1);
-  return categories[random_category_no];
-};
-
-const _getRandomPage = () => {
-  const instagram_page = _getInstagramPageCategory();
-  const random_page_no = generateRandomInteger(0, instagram_page.length - 1);
-  const random_page = `${instagram_page[random_page_no]}`;
-  return random_page;
-};
-
-const automateInstagramStory = async () => {
-  const date_obj = new Date();
-  let hour = date_obj.getHours();
-  hour = minimumIntegerDigits(hour > 12 ? hour - 12 : hour);
-  const minute = minimumIntegerDigits(date_obj.getMinutes());
-  global.schedulerCount = (global.schedulerCount || 0) + 1;
-  const time_log = `Scheduler Running for ${global.schedulerCount} time at ${hour}:${minute}`;
-  console.log(time_log);
-  const fallback_image = `https://thumbs.dreamstime.com/b/computer-error-box-funny-fake-message-i-speechless-original-design-error-box-i-speechless-197648494.jpg`;
-  const fallback_err = "Error while processing!";
-  const _uploadStoryFromUrl = (story_url, is_video) => {
-    ig.uploadStoryFromUrl(story_url, {
-      is_video,
-      callback: async (resp) => {
-        !resp.success && console.log(resp?.error || fallback_err);
-        await ig.exit();
-      },
-    });
-  };
-  const navigateToPage = _getRandomPage();
-  const ig = new InstagramPuppet();
-  ig.setBaseDir(path.relative(process.cwd(), __dirname + "/public/files/"));
-  ig.setFallbackImage(fallback_image);
-  await ig.initialize();
-  const { CLIENT_ID, SECRET_KEY } = process.env;
-  await ig.login(CLIENT_ID, SECRET_KEY);
-  ig.getRandomPostFromPage(navigateToPage)
-    .then((resp) => {
-      console.log("resp =", resp);
-      const is_video = !!resp?.node?.is_video;
-      const story_url = resp?.node?.[!is_video ? "display_url" : "video_url"];
-      _uploadStoryFromUrl(story_url, is_video);
-    })
-    .catch((err) => {
-      console.log(err || fallback_err);
-      _uploadStoryFromUrl(null, null);
-    });
-};
-
-module.exports = { automateInstagramStory };
+module.exports = InstagramPuppet;
